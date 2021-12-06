cb9f3ea1ec809c261ebf0ea5ed649bea2d4c48d6
tiapezz <mattia.pezzolla.mobile@gmail.com>
2021-12-06 16:09:12 +0100

    fix vari
    fix font
    fix roadmap
    fix container
    add slideshow

--- src/app/Desktop/home/home.component.ts
+++ src/app/Desktop/home/home.component.ts
@@ -15,6 +15,20 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {  
     let h = document.getElementById('sec');
     let he = h?.style.height;
+    var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       (x[i] as HTMLElement).style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    (x[myIndex-1] as HTMLElement).style.display = "block";  
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
   }
 
 
@@ -41,6 +55,8 @@ export class HomeComponent implements OnInit {
 
     console.log(value);
   }
+
+
 
 
 }
