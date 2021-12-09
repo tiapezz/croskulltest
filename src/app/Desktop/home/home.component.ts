c4a078a46f1a00c1e335962eb15984bb13a90d76
tiapezz <mattia.pezzolla.mobile@gmail.com>
2021-12-09 16:11:56 +0100

    try header

--- src/app/Desktop/home/home.component.ts
+++ src/app/Desktop/home/home.component.ts
@@ -51,7 +51,7 @@ function carousel() {
     text.style.marginRight = value * 4 + 'px';
     text.style.marginTop= value * 1.5 + 'px';
     btn.style.marginTop= value * 1.5 + 'px';
-    header.style.top = value * 0.5 + 'px';  
+    header.style.top = value * 0.000000000000000000001 + 'px';  
 
     console.log(value);
   }
