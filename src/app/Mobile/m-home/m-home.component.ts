2cb842e626946651521b4d6981fe07093ca26565
tiapezz <mattia.pezzolla.mobile@gmail.com>
2021-12-10 14:45:20 +0100

    fix css and mobile

--- src/app/Mobile/m-home/m-home.component.ts
+++ src/app/Mobile/m-home/m-home.component.ts
@@ -13,22 +13,7 @@ export class MHomeComponent implements OnInit {
   public  height = '';
 
   ngOnInit(): void {  
-    let h = document.getElementById('sec');
-    let he = h?.style.height;
-    var myIndex = 0;
-    carousel();
-    
-    function carousel() {
-        var i;
-        var x = document.getElementsByClassName("mySlides");
-        for (i = 0; i < x.length; i++) {
-           (x[i] as HTMLElement).style.display = "none";  
-        }
-        myIndex++;
-        if (myIndex > x.length) {myIndex = 1}    
-        (x[myIndex-1] as HTMLElement).style.display = "block";  
-        setTimeout(carousel, 2000); // Change image every 2 seconds
-    }
+
   }
 
 
@@ -36,24 +21,20 @@ export class MHomeComponent implements OnInit {
   scrollfunction(){
     let value = document.documentElement.scrollTop;
     /*stars.style.left = value * 0.25 + 'px';*/
-    document.documentElement.getElementsByTagName('stars');
-    let stars = document.getElementById('stars') as HTMLElement;
+    document.documentElement.getElementsByTagName('cloud');
+    let cloud = document.getElementById('cloud') as HTMLElement;
     let moon = document.getElementById('moon') as HTMLElement;
-    let mountains_behind = document.getElementById('mountains_behind') as HTMLElement;
+    let grass = document.getElementById('grass') as HTMLElement;
     let text = document.getElementById('text') as HTMLElement;
     let btn = document.getElementById('btn') as HTMLElement;
-    let mountains_front = document.getElementById('mountains_front') as HTMLElement;
-    let header = document.querySelector('header') as HTMLElement;
 
-    stars.style.top = value * 0.25 + 'px';
+    cloud.style.top = value * 0.25 + 'px';
     moon.style.top = value * 0.9 + 'px';
-    mountains_behind.style.top = value * 0.5 + 'px';
+    grass.style.top = value * 0.5 + 'px';
     text.style.marginRight = value * 4 + 'px';
     text.style.marginTop= value * 1.5 + 'px';
-    btn.style.marginTop= value * 1.5 + 'px';
-    header.style.top = value * 0.5 + 'px';  
+    btn.style.marginTop= value * 1.5 + 'px'; 
 
-    console.log(value);
   }
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,26 +8,4 @@ import { Component, HostListener } from '@angular/core';
 export class AppComponent {
   title = 'NFT-Test';
 
-  @HostListener("document:scroll")
-  scrollfunction(){
-    let value = document.documentElement.scrollTop;
-    /*stars.style.left = value * 0.25 + 'px';*/
-    document.documentElement.getElementsByTagName('stars');
-    let stars = document.getElementById('stars') as HTMLElement;
-    let moon = document.getElementById('moon') as HTMLElement;
-    let mountains_behind = document.getElementById('mountains_behind') as HTMLElement;
-    //let text = document.getElementById('text') as HTMLElement;
-    let btn = document.getElementById('btn') as HTMLElement;
-    let mountains_front = document.getElementById('mountains_front') as HTMLElement;
-    let header = document.querySelector('header') as HTMLElement;
-
-    stars.style.top = value * 0.25 + 'px';
-    moon.style.top = value * 0.9 + 'px';
-    mountains_behind.style.top = value * 0.5 + 'px';
-    //text.style.marginRight = value * 4 + 'px';
-    //text.style.marginTop= value * 1.5 + 'px';
-    btn.style.marginTop= value * 1.5 + 'px';
-    header.style.top = value * 0.5 + 'px';  
-
-  }
 }
