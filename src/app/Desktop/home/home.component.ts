6b0f0599f92f0d7bcff736a31652a019257e05ef
tiapezz <mattia.pezzolla.mobile@gmail.com>
2021-12-13 15:43:26 +0100

    fix vari

--- src/app/Desktop/home/home.component.ts
+++ src/app/Desktop/home/home.component.ts
@@ -49,7 +49,7 @@ export class HomeComponent implements OnInit {
     let cloud = document.getElementById('cloud') as HTMLElement;
     let moon = document.getElementById('moon') as HTMLElement;
     let grass = document.getElementById('grass') as HTMLElement;
-    let text = document.getElementById('text') as HTMLElement;
+    let text = document.getElementById('logo-section') as HTMLElement;
     let btn = document.getElementById('btn') as HTMLElement;
     let graves = document.getElementById('graves') as HTMLElement;
     let header = document.querySelector('header') as HTMLElement;
@@ -57,7 +57,7 @@ export class HomeComponent implements OnInit {
     moon.style.top = value * 0.9 + 'px';
     grass.style.top = value * 0.5 + 'px';
     text.style.marginRight = value * 4 + 'px';
-    text.style.marginTop= value * 1.5 + 'px';
+    text.style.marginTop= value  * 1.5 + 'px';
     btn.style.marginTop= value * 1.5 + 'px';
     header.style.top = value * 0.000000000000000000001 + 'px';  
 
